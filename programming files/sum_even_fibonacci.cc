028194b94981000292fae86594ff56a6c2ecb4ea
Jack Klug <112433250+jjklug@users.noreply.github.com>
2023-03-20 17:35:19 -0400

    Update sum_even_fibonacci.cc
    bugs introduced by jack klug

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 
-//hi
+//Jack Klug bugs introduced
 int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
-        if (b % 2 == 0) {
-            sum_even += b;
+        if (b % 2 != 0) {
+            sum_even -= b;
         }
         int temp = b;
         b = a + b;
